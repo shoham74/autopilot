05ff722924168346cc5044a3c4ed8b1ddfba7528
shoham <shohamiko1@gmail.com>
2023-08-25 15:37:40 +0300

    inc/interpreter.hpp

---
+++ inc/interpreter.hpp
@@ -0,0 +1,21 @@
+#ifndef INTERPRETER_HPP
+#define INTERPRETER_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "parser.hpp"
+#include "blocking_map.hpp"
+
+namespace fg {
+class Interpreter {
+public:
+    void execute(std::string const& a_script);
+private:
+    void execute_AST(std::vector<std::unique_ptr<ASTNode>> const& a_ast_node);
+private:
+    concurrency::BlockingMap<std::string, std::tuple<std::string, std::string, Var>> m_variables;
+};
+} //namespace fg
+#endif // INTERPRETER_HPP
